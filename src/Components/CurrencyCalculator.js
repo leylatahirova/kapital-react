51cd6243a6dc4db7bf377a7c11070c989ebe0b97
tleyla <leylatahirova1@gmail.com>
2024-01-27 14:29:20 +0400

    currency calculator addded

--- src/Components/CurrencyCalculator.js
+++ src/Components/CurrencyCalculator.js
@@ -1,74 +1,124 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "./CurrencyCalculator.css";
 
-
 export default function CurrencyCalculator() {
-    return (
-      <section className="currency-calculator">
-        <div className="calculator-info">
-          <div className="currency-calculator-col1">
-            <div className="currency-calculator-text1">
-              <p className="currency-calculator-h2" style={{ fontSize: "38px", color: "#2e3131" }}>
-                Valyuta <strong style={{ fontWeight: 600, color: "#2e3131" }}>kalkulyatoru</strong>
-              </p>
-              <p className="currency-calculator-p" style={{ fontWeight: 300 }}>
-                Bəzi filiallar üzrə fərqli məzənnələr təyin edilə bilər
-              </p>
-            </div>
-            <table className="currency-calculator-table">
-              <tbody>
-                <tr>
-                  <td className="td-head">Valyuta</td>
-                  <td className="td-head">Alış</td>
-                  <td className="td-head">Satış</td>
-                </tr>
-                <tr>
-                  <td className="td-1">USD</td>
-                  <td className="td-1">1.697</td>
-                  <td className="td-2">1.702</td>
-                </tr>
-                <tr>
-                  <td className="td-1">EUR</td>
-                  <td className="td-1">1.8582</td>
-                  <td className="td-2">1.9037</td>
-                </tr>
-              </tbody>
-            </table>
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('AZN');
+  const [operation, setOperation] = useState('sell');
+  const [isActiveButton1, setIsActiveButton1] = useState(true);
+  const [isActiveButton2, setIsActiveButton2] = useState(false);
+
+  const exchangeRates = {
+    USD: { buy: 1.7, sell: 1.6 },
+    EUR: { buy: 1.9, sell: 1.8 },
+    AZN: { buy: 1, sell: 1 },
+  };
+
+  const [convertedAmount, setConvertedAmount] = useState(amount);
+
+  useEffect(() => {
+    const convertAmount = () => {
+      if (fromCurrency === toCurrency) {
+        setConvertedAmount(amount);
+      } else {
+        const rate = operation === 'sell' ? exchangeRates[fromCurrency].sell : exchangeRates[fromCurrency].buy;
+        setConvertedAmount(amount * rate);
+      }
+    };
+
+    convertAmount();
+  }, [amount, fromCurrency, toCurrency, operation, exchangeRates]);
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleFromCurrencyChange = (e) => {
+    setFromCurrency(e.target.value);
+  };
+
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
+
+  const handleOperationChange = (selectedOperation) => {
+    setOperation(selectedOperation);
+    setIsActiveButton1(selectedOperation === 'sell');
+    setIsActiveButton2(selectedOperation === 'buy');
+  };
+
+  return (
+    <section className="currency-calculator">
+      <div className="calculator-info">
+        <div className="currency-calculator-col1">
+          <div className="currency-calculator-text1">
+            <p className="currency-calculator-h2" style={{ fontSize: "38px", color: "#2e3131" }}>
+              Valyuta <strong style={{ fontWeight: 600, color: "#2e3131" }}>kalkulyatoru</strong>
+            </p>
+            <p className="currency-calculator-p" style={{ fontWeight: 300 }}>
+              Bəzi filiallar üzrə fərqli məzənnələr təyin edilə bilər
+            </p>
           </div>
-          <div className="currency-calculator-col2">
-            <div className="currency-calculator-buttons">
-              <button className="currency-calculator-button1">Mən satıram</button>
-              <button className="currency-calculator-button2">Mən alıram</button>
-            </div>
-            <div className="currency-calculator-drop">
-              <input className="currency-calculator-input" type="text" />
-              <select className="currency-calculator-dropdown">
-                {/* <i className="fa-solid fa-chevron-down" style={{ color: "#ef3d42" }}></i> */}
-                <option value="">AZN</option>
-                <option value="1">USD</option>
-                <option value="0">EUR</option>
-              </select>
-            </div>
-            <p style={{ fontWeight: 300, paddingTop: "10px", paddingBottom: "10px" }}>Məbləği daxil edin</p>
-            <table>
-              <tbody>
-                <tr>
-                  <td className="td-3">58.75</td>
-                  <td className="td-4">USD</td>
-                </tr>
-                <tr>
-                  <td className="td-3">52.92</td>
-                  <td className="td-4">EUR</td>
-                </tr>
-              </tbody>
-            </table>
+          <table className="currency-calculator-table">
+            <tbody>
+              <tr>
+                <td className="td-head">Valyuta</td>
+                <td className="td-head">Alış</td>
+                <td className="td-head">Satış</td>
+              </tr>
+              <tr>
+                <td className="td-1">USD</td>
+                <td className="td-1">{exchangeRates.USD.buy}</td>
+                <td className="td-2">{exchangeRates.USD.sell}</td>
+              </tr>
+              <tr>
+                <td className="td-1">EUR</td>
+                <td className="td-1">{exchangeRates.EUR.buy}</td>
+                <td className="td-2">{exchangeRates.EUR.sell}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="currency-calculator-col2">
+          <div className="currency-calculator-buttons">
+            <button
+              className={`currency-calculator-button1 ${isActiveButton1 ? 'active' : ''}`}
+              onClick={() => handleOperationChange('sell')}
+            >
+              Mən satıram
+            </button>
+            <button
+              className={`currency-calculator-button2 ${isActiveButton2 ? 'active' : ''}`}
+              onClick={() => handleOperationChange('buy')}
+            >
+              Mən alıram
+            </button>
           </div>
+          <div className="currency-calculator-drop">
+            <input className="currency-calculator-input" type="text" value={amount} onChange={handleAmountChange} />
+            <select className="currency-calculator-dropdown" value={fromCurrency} onChange={handleFromCurrencyChange}>
+              {Object.keys(exchangeRates).map(currency => (
+                <option key={currency} value={currency}>{currency}</option>
+              ))}
+            </select>
+          </div>
+          <p style={{ fontWeight: 300, paddingTop: "10px", paddingBottom: "10px" }}>Məbləği daxil edin</p>
+          <table>
+            <tbody>
+              <tr>
+                <td className="td-3">{convertedAmount.toFixed(2)}</td>
+                <td className="td-4">{toCurrency}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div className="currency-information-col1-p3">
-          <a className="information-col1-p3" href="https://www.kapitalbank.az/currency-rates/11-01-2024">
-            Daha ətraflı &nbsp; <i className="fa-solid fa-arrow-right" style={{ color: "#ef3e42" }}></i>
-          </a>
-        </div>
-      </section>
-    );
-  }
+      </div>
+      <div className="currency-information-col1-p3">
+        <a className="information-col1-p3" href="https://www.kapitalbank.az/currency-rates/11-01-2024">
+          Daha ətraflı &nbsp; <i className="fa-solid fa-arrow-right" style={{ color: "#ef3e42" }}></i>
+        </a>
+      </div>
+    </section>
+  );
+}
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,56 +1,57 @@
-import React from 'react'
-import "./News.css";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import './News.css';
 
 export default function News() {
+  const newsItems = [
+    { day: 30, month: "Dekabr", content: "Birbank Biznes yeni 'Nağd təminatlı qarantiya' məhsulunu təqdim edir" },
+    { day: 30, month: "Dekabr", content: "Birbank Biznes yeni 'Nağd təminatlı qarantiya' məhsulunu təqdim edir" },
+    { day: 30, month: "Dekabr", content: "Birbank Biznes yeni 'Nağd təminatlı qarantiya' məhsulunu təqdim edir" }
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % newsItems.length);
+  };
+
   return (
     <section className="latest-news">
       <div className="news-container">
         <div className="news-heading">
           <h1 className="news-h1">Xəbərlər</h1>
-          <p className="news-p">Bütün xəbərlər&nbsp;<i className="fa-solid fa-arrow-right" style={{ color: "#f45369" }}></i></p>
+          <p className="news-p">
+            Bütün xəbərlər&nbsp;
+            <FontAwesomeIcon icon={faChevronRight} style={{ color: "#f45369", }} onClick={handleNext} />
+          </p>
         </div>
         <div className="news-line"></div>
         <div className="news-col">
-          <div className="news-col1">
-            <div className="news-col-left">
-              <div className="news-circle">
-                <p className="news-circle-p1">30</p>
+          {newsItems.map((item, index) => (
+            <div
+              className={`news-col2 ${index === activeIndex ? 'active' : ''}`} key={index}
+            >
+              <div className="news-col-left">
+                <div className="news-circle">
+                  <p className="news-circle-p1">{item.day}</p>
+                </div>
+                <div>
+                  <p className="news-circle-p2">{item.month}</p>
+                </div>
               </div>
               <div>
-                <p className="news-circle-p2">Dekabr</p>
+                <p className="news-p2">{item.content}</p>
               </div>
             </div>
-            <div>
-              <p className="news-p2">Birbank Biznes yeni "Nağd təminatlı qarantiya" məhsulunu təqdim edir</p>
-            </div>
-          </div>
-          <div className="news-col2">
-            <div className="news-col-left">
-              <div className="news-circle">
-                <p className="news-circle-p1">30</p>
-              </div>
-              <div>
-                <p className="news-circle-p2">Dekabr</p>
-              </div>
-            </div>
-            <div>
-              <p className="news-p2">Birbank Biznes yeni "Nağd təminatlı qarantiya" məhsulunu təqdim edir</p>
-            </div>
-          </div>
-          <div className="news-col2">
-            <div className="news-col-left">
-              <div className="news-circle">
-                <p className="news-circle-p1">30</p>
-              </div>
-              <div>
-                <p className="news-circle-p2">Dekabr</p>
-              </div>
-            </div>
-            <div>
-              <p className="news-p2">Birbank Biznes yeni "Nağd təminatlı qarantiya" məhsulunu təqdim edir</p>
-            </div>
-          </div>
-          <i className="fa-solid fa-chevron-right" style={{ color: "#bdc3c7", fontSize: "26px" }}></i>
+          ))}
+         
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            style={{ color: "#bdc3c7", fontSize: "26px" }}
+            onClick={handleNext}
+          />
+         
         </div>
       </div>
     </section>
